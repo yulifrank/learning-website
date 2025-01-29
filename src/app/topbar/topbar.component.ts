48a463f06b9fbc2b56a7357b584a56ba55b3e222
yulifrank <148456398+yulifrank@users.noreply.github.com>
2025-01-29 15:10:41 +0200

    fix responsive

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
@@ -9,9 +9,20 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent {
+  constructor(private router: Router) {}
+
   menuOpen = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
+
+  scrollToContact(): void {
+    const contactElement = document.getElementById('contact');
+    if (contactElement) {
+      contactElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  
+ 
 }
