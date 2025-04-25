2e8c121e81a9b01cbb223339bb51031954932db8
yulifrank <148456398+yulifrank@users.noreply.github.com>
2025-04-25 15:04:05 +0300

    qutes.

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,17 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    [
-      { text: "Talk is cheap. Show me the code.", author: "Linus Torvalds" },
-      { text: "Programs must be written for people to read, and only incidentally for machines to execute.", author: "Harold Abelson" },
-      { text: "Don't comment bad code—rewrite it.", author: "Brian Kernighan" },
-      { text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", author: "Martin Fowler" },
-      { text: "First, solve the problem. Then, write the code.", author: "John Johnson" },
-      { text: "Simplicity is the soul of efficiency.", author: "Austin Freeman" },
-      { text: "The best programs are written so that computing machines can perform them quickly and so that human beings can understand them clearly.", author: "Donald Knuth" },
-      { text: "It’s harder to read code than to write it.", author: "Joel Spolsky" }
-    ]
-    
+    { text: "Talk is cheap. Show me the code.", author: "Linus Torvalds" },
+    { text: "Programs must be written for people to read, and only incidentally for machines to execute.", author: "Harold Abelson" },
+    { text: "Don't comment bad code—rewrite it.", author: "Brian Kernighan" },
+    { text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", author: "Martin Fowler" },
+    { text: "First, solve the problem. Then, write the code.", author: "John Johnson" },
+    { text: "Simplicity is the soul of efficiency.", author: "Austin Freeman" },
+    { text: "The best programs are written so that computing machines can perform them quickly and so that human beings can understand them clearly.", author: "Donald Knuth" },
+    { text: "It’s harder to read code than to write it.", author: "Joel Spolsky" }
   ];
 
   currentQuote = 0;
